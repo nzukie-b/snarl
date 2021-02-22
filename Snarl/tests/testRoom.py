4bcfcd8fccc794c99503935c2f63c83d9e371d19
nzukie <nzukie.b@husky.neu.edu>
2021-02-22 13:45:37 -0500

    Initial testRoom

---
+++ Snarl/tests/testRoom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+
+class Bounds:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.columns = cols
+
+class Room:
+    def __init__(self, origin, bounds, layout):
+        self.type =  'room'
+        self.origin = origin
+        self.bounds = bounds
+        self.layout = layout
+
+
