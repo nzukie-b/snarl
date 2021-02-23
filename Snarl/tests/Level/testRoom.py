02dee947527f1486b3bf4d271cf89f0d17e91e31
nzukie <nzukie.b@husky.neu.edu>
2021-02-22 20:43:20 -0500

    Start of tetstRoom

--- Snarl/tests/Level/testRoom.py
+++ Snarl/tests/Level/testRoom.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import json
-import os
-
+import sys, os, json
+currentdir = os.path.dirname(os.path.realpath(__file__))
+tests_dir = os.path.dirname(currentdir)
+snarl_dir = os.path.dirname(tests_dir)
+game_dir = snarl_dir + '/Game'
+from coord import Coord
+from room import Room
 
 class Bounds:
     def __init__(self, rows, cols):
@@ -16,4 +20,20 @@ class Room:
         self.bounds = bounds
         self.layout = layout
 
+
+
+def main():
+    room_json = input()
+    room_obj = json.loads(room_json)
+    if room_obj['type'] != 'room':
+        print('Invalid Args: Type is not room')
+        return False
+    origin = room_obj['origin']
+    bounds = room_obj['bounds']
+    layout = room_obj['layout']
+    # TODO: Not sure if this order will be consistent
+    point = room_obj[4]
+    origin_coord = Coord(origin[0], origin[1])
+    dimensions = Coord(bounds['columns'], bounds['rows'])
+    # for i in range(0, bounds['col'])
 
