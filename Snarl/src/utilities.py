4234bad80d5e4eb360aad6d18ea2ec82ac0c3f28
nzukie <nzukie.b@husky.neu.edu>
2021-03-02 13:00:42 -0500

    Parsing for hallway json input

--- Snarl/src/utilities.py
+++ Snarl/src/utilities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from hallway import Hallway
 import json
 from coord import Coord
 from room import Room
@@ -60,7 +61,8 @@ def check_level(level):
             return False
     return level.check_level_dimensions()
 
-def parse_room(room_input):
+
+def parse_room_obj(room_input):
     room_json = json.loads(room_input)
     if room_json[0]['type'] != 'room':
         print(room_json['type'])
@@ -69,7 +71,14 @@ def parse_room(room_input):
     origin = room_json[0]['origin']
     bounds = room_json[0]['bounds']
     layout = room_json[0]['layout']
-    point = room_json[1]
+    point = None
+    
+    try: 
+        point = room_json[1]
+    except IndexError:
+        # Don't want to do anything here since there as room input does not need to have a point
+        pass
+
     origin_coord = Coord(origin[0], origin[1])
     dimensions = Coord(bounds['rows'], bounds['columns'])
     tiles = []
@@ -81,13 +90,66 @@ def parse_room(room_input):
                 tiles.append(Coord(origin[0] + ii, origin[1] + jj))
             if layout[ii][jj] == 2:
                 doors.append(Coord(origin[0] + ii, origin[1] + jj))
-    room_obj = Room(origin_coord, dimensions, tiles, doors)
-    if (point[0] not in range(origin_coord.x, origin_coord.x + dimensions.x + 1)) or (point[1] not in range(origin_coord.y, origin_coord.y + dimensions.y + 1)):
-        print('[ Failure: Point ", {} , " is not in room at ", {} ]'.format(point, origin))
+    return {'point': point, 'room': Room(origin_coord, dimensions, tiles, doors)}
+
+
+def parse_room(room_input):
+    parsed_input = parse_room_obj(room_input)
+    point = parsed_input['point']
+    if not point:
+        print('Invalid Args: point is missing')
+        return False
+    room_obj = parsed_input['room']
+    if (point[0] not in range(room_obj.origin.x, room_obj.origin.x + room_obj.dimensions.x + 1)) or (point[1] not in range(room_obj.origin.y, room_obj.origin.y + room_obj.dimensions.y + 1)):
+        print('[ Failure: Point ", {} , " is not in room at ", {} ]'.format(point, [room_obj.origin.x, room_obj.origin.y]))
         return False
     reachable_coords = room_obj.get_reachable_tiles(Coord(point[0], point[1]))
     reachable_tiles = []
     for coord in reachable_coords:
         reachable_tiles.append([coord.x, coord.y])
-    print('[ Success: Traversable points from, " {} ," in room at ", {}, " are ", {} ]'.format(point, origin, reachable_tiles))
+    print('[ Success: Traversable points from, " {} ," in room at, " {} ," are, " {} ]'.format(point, [room_obj.origin.x, room_obj.origin.y], reachable_tiles))
     return reachable_tiles
+    
+
+def parse_hall(hall_input, rooms):
+    hall_json = json.loads(hall_input)
+    if hall_json['type'] != 'hallway':
+        print('Invalid Args: Type is not hallway')
+        return False
+    from_ = hall_json['from']
+    to = hall_json['to']
+    from_coord = Coord(from_[0], from_[1])
+    to_coord = Coord(to[0], to[1])
+    waypoints = hall_json['waypoints']
+    waypoints_coords = [Coord(waypoint[0], waypoint[1]) for waypoint in waypoints]
+    
+    is_horizontal = from_coord.y == to_coord.y and from_coord.x != to_coord.x
+    hall_boundaries = None
+    origin = None
+    dimensions = None
+    if is_horizontal:
+        # From and To a tiles within a room so hall boundaries do not include them
+        hall_boundaries = (min(from_coord.x, to_coord.x) + 1, max(from_coord.x, to_coord.x) - 1)
+        origin = Coord(hall_boundaries[0], from_coord.y)
+        # For now halls are stated to only be a single tile wide
+        dimensions = Coord(hall_boundaries[1] - hall_boundaries[0], 1)
+    else:
+        hall_boundaries = (min(from_coord.y, to_coord.y) + 1, max(from_coord.y, to_coord.y) - 1)
+        origin = Coord(from_coord.x, hall_boundaries[0])
+        dimensions = Coord(1, hall_boundaries[1] - hall_boundaries[0])
+    
+    rooms_list = []
+    waypoints_list = []
+    for room in rooms:
+        for door in room.doors:
+            to_room = None
+            from_room = None
+            if door == to_coord or door == from_coord:
+                rooms_list.append(room)
+            # Doors are considered to be inside a room each waypoint coord should correspond to a door in a room.
+            for waypoint in waypoints_coords:
+                if door == waypoint:
+                    waypoints_list.append(room)
+    return Hallway(origin, dimensions, rooms, waypoints_list)
+    
+        
