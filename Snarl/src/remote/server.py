25aa6f2b660020ec53a0661aa38d38ff7258a975
nzukie <nzukie.b@husky.neu.edu>
2021-04-19 15:03:46 -0400

    Initial work for multi-threaded server

--- Snarl/src/remote/server.py
+++ Snarl/src/remote/server.py
@@ -1,5 +1,6 @@
 import sys, os, argparse, time, json, math
 import socket
+import threading
 from pathlib import Path
 from typing import List
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -17,13 +18,13 @@ from view.view import render_state
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--levels', dest='levels', action='store', default='snarl.levels', help="Location of local levels file")
-parser.add_argument('-c', '--clients', dest='clients', action='store', type=int, default=1, help='Number of players')
+parser.add_argument('-c', '--clients', dest='clients', action='store', type=int, default=1, help='Number of players allowed per game')
 parser.add_argument('-w', '--wait', dest='wait', action='store', type=int, default=20, help='Number of seconds to wait for next player to connect before connection timeout')
 parser.add_argument('-s', '--start', dest='start', action='store', type=int, default=1, help='Start level. Not 0 indexed')
 parser.add_argument('-o', '--observe', dest='observe', action='store_true', default=False, help='Whether to return observer view of the ongoing game.')
 parser.add_argument('-a', '--address', dest='address', action='store', default='127.0.0.1', help='Address to start listing for connections')
 parser.add_argument('-p', '--port', dest='port', action='store', type=int, default=45678, help='Port to start listing for connections')
-
+parser.add_argument('-t', '--threads', dest='threads', action='store', type=int, default=2, help='Max number of concurrent games')
 
 def __valid_clients_num(no_clients):
     return no_clients <= MAX_PLAYERS
@@ -156,26 +157,7 @@ def __update_observer(observe: bool, state: GameState):
     if observe:
         render_state(state)
 
-def main(args):
-    if not __valid_clients_num(args.clients):
-        print('Invalid number of clients. Please enter a number between 1 and {}'.format(MAX_PLAYERS))
-        exit()
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_addr = (args.address, args.port)
-    server_socket.bind(server_addr)
-    server_socket.listen(MAX_PLAYERS)
-    
-    no_clients = args.clients
-    levels_file = Path(args.levels).read_text()
-    levels_list = parse_levels(levels_file)
-    start_level = args.start
-    observe = args.observe
-
-    clients = __wait_for_client_connections(server_socket, server_addr, no_clients, args.wait)
-    __close_invalid_clients(clients)
-
-    gm = GameManager()
+def thread(gm: GameManager, clients, levels_list, start_level, observe):
     __register_players(gm, clients)
     __register_adversaries(gm, len(levels_list))
    
@@ -227,6 +209,33 @@ def main(args):
                 break
 
     __send_end_game(gm.players, player_scores)
+
+
+def main(args):
+    if not __valid_clients_num(args.clients):
+        print('Invalid number of clients. Please enter a number between 1 and {}'.format(MAX_PLAYERS))
+        exit()
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = (args.address, args.port)
+    server_socket.bind(server_addr)
+    server_socket.listen(MAX_PLAYERS)
+    
+    no_clients = args.clients
+    levels_file = Path(args.levels).read_text()
+    levels_list = parse_levels(levels_file)
+    start_level = args.start
+    observe = args.observe
+
+    clients = __wait_for_client_connections(server_socket, server_addr, no_clients, args.wait)
+    __close_invalid_clients(clients)
+    #clients[start:stop] range(0, len() step) = [0, n + step...]
+    client_threads = [clients[i: i + no_clients] for i in range(0, len(clients), no_clients)]
+    for ct in client_threads:
+        gm = GameManager()
+        __register_players(gm, ct)
+        __register_adversaries(gm, len(levels_list))
+        thread(gm, ct, levels_list, start_level)
     __close_connections(gm.players, server_socket)
 
                 
@@ -234,4 +243,4 @@ def main(args):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args)
+    thread(args)
